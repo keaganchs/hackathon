ab1e2e58c38e700ed852b350f59c4f366c8797f9
Keagan Holmes <keaganchs@gmail.com>
2022-11-05 17:58:05 +0100

    Remove users

--- backend/server.py
+++ backend/server.py
@@ -56,42 +56,6 @@ def add_test_data(db: Session = Depends(get_db)):
     except:
         return "Unable to add test data."
 
-# Users
-########################################################
-
-@app.post("/users/", response_model=pydantic_models.User, tags={"users"})
-def create_user(user: pydantic_models.UserCreate, db: Session = Depends(get_db)):
-    db_user = users.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return users.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=List[pydantic_models.User], tags={"users"})
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return users.get_users(db, skip=skip, limit=limit)
-
-
-@app.get("/users/{user_id}", response_model=pydantic_models.User, tags={"users"})
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = users.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=pydantic_models.Item, tags={"users"})
-def create_item_for_user(
-    user_id: int, item: pydantic_models.ItemCreate, db: Session = Depends(get_db)
-):
-    return users.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=List[pydantic_models.Item], tags={"users"})
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = users.get_items(db=db, skip=skip, limit=limit)
-    return items
-
 # Cities
 ########################################################
 
